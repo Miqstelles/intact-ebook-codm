686ceecb96f3f8f07c90a266c832898cbc12d131
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-02-04 00:31:34 -0300

    add button component

---
+++ src/components/Btns/Button.tsx
@@ -0,0 +1,32 @@
+export function Button() {
+    return (
+        <button className="
+        w-[296px] h-[50px] bg-gr-100 font-sora font-bold text-[16px] rounded-[5px] 
+        hover:bg-transparent hover:border-gr-100 hover:border-[3px] hover:text-white duration-500"
+        >
+            GARANTIR EBOOK
+        </button>
+    )
+}
+
+export function ButtonFooter() {
+    return (
+        <button className="
+        w-[267px] h-[54px] bg-gr-100 font-sora font-bold text-[16px] rounded-[5px] 
+        hover:bg-transparent hover:border-gr-100 hover:border-[3px] hover:text-white duration-500"
+        >
+            GARANTIR EBOOK
+        </button>
+    )
+}
+
+export function ButtonHeader() {
+    return (
+        <button className="
+        w-[328px] h-[62px] bg-transparent text-[#B0B0B0] font-sora font-bold text-[20px] rounded-[5px] border-gr-100 border-[3px] duration-500
+        hover:text-white"
+        >
+            GARANTIR EBOOK
+        </button>
+    )
+}
