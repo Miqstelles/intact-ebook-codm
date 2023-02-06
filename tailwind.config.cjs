3fb76099cbfb9b7bc49f250efc7fdee3eceb4e91
Miqstelles <90120169+Miqstelles@users.noreply.github.com>
2023-02-06 04:40:20 -0300

    add fade-in-down animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,6 +16,21 @@ module.exports = {
         'prp-200': '#0E0816',
         'prp-300': '#231633'
       },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down .5s ease-out'
+      }
     },
   },
   plugins: [],
